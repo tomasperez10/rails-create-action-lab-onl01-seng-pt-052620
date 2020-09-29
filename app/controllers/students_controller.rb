742e66082729427855e684927e2c931e9a18e157
Tomas Perez <tomasitopg6@gmail.com>
2020-09-29 16:40:06 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -15,7 +15,7 @@ class StudentsController < ApplicationController
     @student = Student.new
     @student.first_name = params[:first_name]
     @student.last_name = params[:last_name]
-    @post.save
+    @student.save
     redirect_to post_path(@post)
   end
 
